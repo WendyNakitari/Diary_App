79fec074b7cbfec5b30f6b2062ca3fb3ba185ed2
WendyNakitari <70348582+WendyNakitari@users.noreply.github.com>
2020-10-27 18:56:59 +0300

    Add files via upload

---
+++ entries/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Entry
+
+class Entryform(ModelForm):
+    class Meta:
+        model = Entry
+        fields = ('text,' )
+    def _init_(self, *args, **kwargs):
+        super()._init_(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({'class' : 'textarea', 'placeholder' : 'What\'s on your mind?'})
